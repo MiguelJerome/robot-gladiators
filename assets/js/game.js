b2c65df3e2ad5fcc42c745a1b3050b4f48cee4cd
MiguelJerome <sharkmaycry@gmail.com>
2022-06-11 19:02:03 -0400

    add enemy

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+var playerMoney = 10;
+
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+for(var i = 0; i < enemyNames.length; i++)
+{
+    console.log("array :" + enemyNames);
+} 
